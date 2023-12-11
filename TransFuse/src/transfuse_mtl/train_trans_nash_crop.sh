ef676fed08cd72e230a98c6b5291b70fe3090fbe
kazu-gor <ik126@icloud.com>
2023-12-12 04:44:03 +0900

    modified: directory

--- TransFuse/src/transfuse_mtl/train_trans_nash_crop.sh
+++ TransFuse/src/transfuse_mtl/train_trans_nash_crop.sh
@@ -1,3 +1,4 @@
+export PYTHONPATH="../../"
 SAVE_PATH='TransFuse_nash_mtl_crop_original'
 python3 ./train_trans_nash.py --train_save $SAVE_PATH --batchsize 8 --train_path ../../dataset/clipping_20231212_original/TrainDataset --val_path ../../dataset/clipping_20231212_original/ValDataset
 python3 ./test_trans_nash.py --pth_path "./snapshots/$SAVE_PATH/Transfuse-best.pth" --pth_path2 "./snapshots/$SAVE_PATH/Discriminator-best.pth" --data_path1 ../../dataset/clipping_20231212_original/TestDataset --data_path2 ../../dataset/clipping_20231212_original/sekkai_TestDataset
