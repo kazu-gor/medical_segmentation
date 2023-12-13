b8707a27b5f03f4a93fa0867b4e55e0fc514e54f
kazu-gor <ik126@icloud.com>
2023-12-13 15:41:27 +0900

    modified: TRAIN_SAVE_PATH -> SAVE_PATH

--- TransFuse/src/transcaranet_mtl/trans_caranet_mtl_clip.sh
+++ TransFuse/src/transcaranet_mtl/trans_caranet_mtl_clip.sh
@@ -1,7 +1,7 @@
 export PYTHONPATH="../../"
 
 DATA_PATH='clipping_20231212'
-TRAIN_SAVE_PATH='trans_caranet_nash_clip'
+SAVE_PATH='trans_caranet_nash_clip'
 python3 train_trans_caranet_nash.py --train_save $SAVE_PATH --train_path "../../dataset/$DATA_PATH/TrainDataset" --val_path "../../dataset/$DATA_PATH/ValDataset"
 python3 test_trans_caranet_nash.py --pth_path "./snapshots/$SAVE_PATH/Transfuse-best.pth" --pth_path2 "./snapshots/$SAVE_PATH/Discriminator-best.pth" --test_path1 "../../dataset/$DATA_PATH/TestDataset" --test_path2 "../../dataset/$DATA_PATH/sekkai_TestDataset"
 python3 test_trans_caranet_nash.py --pth_path "./snapshots/$SAVE_PATH/Transfuse-best2.pth" --pth_path2 "./snapshots/$SAVE_PATH/Discriminator-best2.pth" --test_path1 "../../dataset/$DATA_PATH/TestDataset" --test_path2 "../../dataset/$DATA_PATH/sekkai_TestDataset"
