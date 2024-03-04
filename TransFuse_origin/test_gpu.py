42a06450509473325b51fcdda7c630334343c5b0
kazu-gor <ik126@icloud.com>
2024-03-04 17:27:24 +0900

    test_gpu.py: add main function to execute test_gpu() when the script is run directly
    test_gpu.py: add empty lines for better readability

--- TransFuse_origin/test_gpu.py
+++ TransFuse_origin/test_gpu.py
@@ -6,3 +6,7 @@ def test_gpu():
     print(torch.cuda.device_count())
     print(torch.cuda.get_device_name(0))
     print(torch.cuda.current_device())
+
+
+if __name__ == "__main__":
+    test_gpu()
