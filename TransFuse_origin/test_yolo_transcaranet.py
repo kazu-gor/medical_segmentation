1012d4241bc6c77dc3df437e3b2c056a1f381d4d
kazu-gor <ik126@icloud.com>
2024-03-26 13:41:18 +0900

    chore(test_yolo_transcaranet.py): add predict_yolo_polyp491 function to test and predict polyp detection using YOLO model feat(test_yolo_transcaranet.py): add predict_yolo_polyp491 function to test and predict polyp detection using YOLO model

--- TransFuse_origin/test_yolo_transcaranet.py
+++ TransFuse_origin/test_yolo_transcaranet.py
@@ -1,3 +1,6 @@
+import glob
+
+from ultralytics import YOLO
 from ultralytics.models.yolo.detect import DetectionPredictor
 
 
@@ -23,5 +26,28 @@ def test_yolo_polyp491():
     predictor.predict_cli()
 
 
+def predict_yolo_polyp491(weights):
+    model = YOLO(weights)
+
+    root_path = '../../../dataset_v0/TestDataset/images'
+    img_files = glob.glob(f'{root_path}/*.png')
+
+    for img_file in img_files:
+        model.predict(
+            img_file,
+            single_cls=True,
+            imgsz=640,
+            max_det=1,
+            conf=0.01,
+            save=True,
+            save_txt=True,
+            save_conf=True,
+            save_crop=True,
+        )
+
+
 if __name__ == '__main__':
-    test_yolo_polyp491()
+    # test_yolo_polyp491()
+
+    predict_yolo_polyp491(
+        'ultralytics/runs/detect/polyp491_18/weights/last.pt')
