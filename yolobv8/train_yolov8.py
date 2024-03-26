ffef5b5eb245751328dae5729a34c9bee9b501f4
kazu-gor <ik126@icloud.com>
2024-03-26 14:09:16 +0900

    refactor(train_yolov8.py): remove unnecessary code for loading model from YAML and transferring weights feat(train_yolov8.py): update model instantiation to only load 'yolov8n.pt' weights feat(train_yolov8.py): update model training parameters to use 'polyp491.yaml' data, 100 epochs, and image size of 640

--- yolobv8/train_yolov8.py
+++ yolobv8/train_yolov8.py
@@ -1,7 +1,6 @@
 from ultralytics import YOLO
 
 # Load a model
-model = YOLO('polyp491.yaml').load('yolov8n.pt')  # build from YAML and transfer weights
-
+model = YOLO('yolov8n.pt')
 # Train the model
 results = model.train(data='polyp491.yaml', epochs=100, imgsz=640)
