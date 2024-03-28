bae665d51b22f495fda175ebeb0bdedc8748df33
kazu-gor <ik126@icloud.com>
2024-03-28 13:21:08 +0900

    chore(test_yolo_transcaranet.py): add image cropping and bounding box drawing functionality to improve data preprocessing feat(test_yolo_transcaranet.py): add support for processing multiple images and labels in a loop to automate the cropping and bounding box drawing process

--- TransFuse_origin/test_yolo_transcaranet.py
+++ TransFuse_origin/test_yolo_transcaranet.py
@@ -1,4 +1,8 @@
 import glob
+import cv2
+from pathlib import Path
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 from ultralytics import YOLO
 from ultralytics.models.yolo.detect import DetectionPredictor
@@ -46,8 +50,36 @@ def predict_yolo_polyp491(weights):
         )
 
 
+def crop_image(img_path, label_path):
+
+    assert Path(img_path).stem == Path(label_path).stem
+    img = cv2.imread(img_path)
+    h, w, _ = img.shape
+    with open(label_path, 'r') as f:
+        lines = f.readlines()
+    for line in lines:
+        line = line.strip().split(' ')
+        _, x, y, w, h = line
+        x, y, w, h = float(x), float(y), float(w), float(h)
+        x, y, w, h = int(x * w), int(y * h), int(w ** 2), int(h ** 2)
+        x1, y1 = x - w // 2, y - h // 2
+        x2, y2 = x + w // 2, y + h // 2
+        crop_img = img[y1:y2, x1:x2]
+        cv2.imwrite(
+            f'./datasets/preprocessing/images/{Path(img_path).name}', crop_img)
+        # draw bounding box
+        cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
+        cv2.imwrite(
+            f'./datasets/preprocessing/plottings/{Path(img_path).name}', img)
+
+
 if __name__ == '__main__':
     # test_yolo_polyp491()
 
     predict_yolo_polyp491(
         'ultralytics/runs/detect/polyp491_18/weights/last.pt')
+
+    pred_path = Path('ultralytics/runs/detect/predict7')
+    gt_path = Path('./dataset_v0/sekkai_TestDataset/images/')
+    for img_file, label_file in zip(sorted(gt_path.glob('*.png')), sorted((pred_path / 'labels').glob('*.txt'))):
+        crop_image(img_file, label_file)
