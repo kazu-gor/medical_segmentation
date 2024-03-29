0edb2a3293e8515994ef737563cd9606da14e5d0
kazu-gor <ik126@icloud.com>
2024-03-29 13:54:17 +0900

    chore(train_yolo_transcaranet.py): add print statement to display the shape of the image and ground truth (gt) arrays for debugging purposes

--- TransFuse_origin/train_yolo_transcaranet.py
+++ TransFuse_origin/train_yolo_transcaranet.py
@@ -54,6 +54,7 @@ def train_yolo(mode, pretrainer: DetectionTrainer):
                 image = cv2.imread(original_img_path)
                 gt = cv2.imread(original_gt_path, 0)
 
+            print(f"Image shape: {image.shape}, GT shape: {gt.shape}")
             image = cv2.resize(image, (352, 352))
             gt = cv2.resize(gt, (352, 352))
 
