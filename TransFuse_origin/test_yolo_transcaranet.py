b135f6e2c722e3471cd700cd82b19a5d1b8993b3
kazu-gor <ik126@icloud.com>
2024-03-29 11:02:29 +0900

    chore(test_yolo_transcaranet.py): remove commented out code and unused imports to improve code readability and maintainability

--- TransFuse_origin/test_yolo_transcaranet.py
+++ TransFuse_origin/test_yolo_transcaranet.py
@@ -4,29 +4,6 @@ import pathlib
 from pathlib import Path
 
 from ultralytics import YOLO
-# from ultralytics.models.yolo.detect import DetectionPredictor
-
-
-# def test_yolo_polyp491():
-#     args = dict(
-#         model='ultralytics/runs/detect/polyp491_34/weights/last.pt',
-#         data='polyp491.yaml',
-#         single_cls=True,
-#         imgsz=640,
-#         batch=1,
-#         workers=4,
-#         name='polyp491_',
-#         save=True,
-#         max_det=1,
-#         # conf=0.01,
-#         source='../../../dataset_v0/TestDataset/images',
-#         save_txt=True,
-#         save_conf=True,
-#         save_crop=True,
-#     )
-
-#     predictor = DetectionPredictor(overrides=args)
-#     predictor.predict_cli()
 
 
 class Predictor:
@@ -164,7 +141,6 @@ class Predictor:
 
 
 if __name__ == '__main__':
-    # test_yolo_polyp491()
     
     predictor = Predictor(
             weights='ultralytics/runs/detect/polyp491_34/weights/last.pt',
