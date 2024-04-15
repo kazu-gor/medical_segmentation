e73478968e8c83f64b5cbc9bb6bda12043eed071
kazu-gor <ik126@icloud.com>
2024-04-15 09:32:21 +0900

    fixed (test_yolo.py): Set a lower limit of 32 x 32 for bounding boxes

--- TransFuse_origin/test_yolo.py
+++ TransFuse_origin/test_yolo.py
@@ -158,14 +158,14 @@ class Predictor:
             x2, y2 = x + w // 2, y + h // 2
             crop_img = img[y1:y2, x1:x2]
 
-            # while crop_img.shape[1] < 32:
-            #     x1 = x1 - (33 - crop_img.shape[1]) // 2
-            #     x2 = x2 + (33 - crop_img.shape[1]) // 2
-            #     crop_img = img[y1:y2, x1:x2]
-            # while crop_img.shape[0] < 32:
-            #     y1 = y1 - (33 - crop_img.shape[0]) // 2
-            #     y2 = y2 + (33 - crop_img.shape[0]) // 2
-            #     crop_img = img[y1:y2, x1:x2]
+            while crop_img.shape[1] < 32:
+                x1 = x1 - (33 - crop_img.shape[1]) // 2
+                x2 = x2 + (33 - crop_img.shape[1]) // 2
+                crop_img = img[y1:y2, x1:x2]
+            while crop_img.shape[0] < 32:
+                y1 = y1 - (33 - crop_img.shape[0]) // 2
+                y2 = y2 + (33 - crop_img.shape[0]) // 2
+                crop_img = img[y1:y2, x1:x2]
 
             crop_img = cv2.resize(crop_img, (352, 352))
 
