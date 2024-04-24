26e748033da5301426f367c415292306cf2d64ce
kazu-gor <ik126@icloud.com>
2024-04-24 10:43:47 +0900

    fixed(utils/dataloader.py): Commented out unlabeled mask processing.

--- TransFuse_origin/utils/dataloader.py
+++ TransFuse_origin/utils/dataloader.py
@@ -227,15 +227,20 @@ class PolypDataset(data.Dataset):
         for img_path, gt_path in zip(self.images, self.gts):
             img = Image.open(img_path)
             gt = Image.open(gt_path)
+
+            # if img.size == gt.size:
+            #     # If gt does not contain any labels (total value is 0), do not include it in the data set
+            #     if self.phase in ['train', 'val']:
+            #         if np.sum(np.array(gt)) > 0:
+            #             images.append(img_path)
+            #             gts.append(gt_path)
+            #     else:
+            #         images.append(img_path)
+            #         gts.append(gt_path)
+
             if img.size == gt.size:
-                # If gt does not contain any labels (total value is 0), do not include it in the data set
-                if self.phase in ['train', 'val']:
-                    if np.sum(np.array(gt)) > 0:
-                        images.append(img_path)
-                        gts.append(gt_path)
-                else:
-                    images.append(img_path)
-                    gts.append(gt_path)
+                images.append(img_path)
+                gts.append(gt_path)
         self.images = images
         self.gts = gts
 
